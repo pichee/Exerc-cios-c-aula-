e5d5d7a87f4d42fef235060762a7c5475888506e
Gustavo Pich <76635390+gustavoalemao@users.noreply.github.com>
2024-03-24 11:00:52 -0300

    exercicio2.c

---
+++ exercicio2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b;
+    printf("digite o primeiro valor:\n");
+    scanf("%f", &b);
+    printf("digite o segundo valor\n");
+    scanf("%f", &a);
+    printf("os números invertidos são %f %f ",a,b);
+
+    
+}
