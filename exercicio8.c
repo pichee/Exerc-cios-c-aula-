7b8028fe1cf5e0df4df3c82808642fd27950c5bd
Gustavo Pich <76635390+gustavoalemao@users.noreply.github.com>
2024-03-24 14:11:38 -0300

    Create exercicio8.c

---
+++ exercicio8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,a;
+    printf("informe sua idade ");
+    scanf("%i",&i);
+    printf("a idade é %i\n",i);
+    a=i*2;
+    printf("a dobro é %i\n",a);
+    a=i*3;
+    printf("a triplo é %i\n",a);
+    return 0;
+}
