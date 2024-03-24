477bcbd1348f3d8c1ae4fa5e8753913b88c06840
Gustavo Pich <76635390+gustavoalemao@users.noreply.github.com>
2024-03-24 14:32:28 -0300

    Create exercicio9.c

---
+++ exercicio9.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+int main(){
+    int i=0;
+    float id,a=0;
+        while (i < 4){
+            printf("digite uma idade");
+            scanf("%f",&id);
+            a=a+id;
+            i++;
+            
+                    }
+    a=a/4;
+    printf("a média de suas idades é igual a %2.f",a);    
+        
+        
+    }
