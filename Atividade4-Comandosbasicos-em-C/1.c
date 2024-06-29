1c21b2346caeeb13dd97681432b18a9e247a863c
Gustavo <76635390+pichee@users.noreply.github.com>
2024-06-29 14:12:54 -0300

    Create 1.c

---
+++ Atividade4-Comandosbasicos-em-C/1.c
@@ -0,0 +1,24 @@
+[09:31, 29/06/2024] Gustavo Pich: D55
+[10:56, 29/06/2024] Gustavo Pich: //Escreva um programa em C que receba um número inteiro e imprima na tela “F1”, “F2” ou “F3”, 
+//conforme as condições abaixo
+//a) F1 se n <= 10
+//b) F2 se n > 10 e n <= 100
+//c) F3 se n > 100
+#include <stdio.h>
+int main(){
+int n;
+printf("Digite um número:");
+scanf("%i",&n);
+
+if(n<=10){
+printf("F1");
+}
+else if(n > 10 & n <= 100){
+printf("F2");
+}
+else if(n>100){
+printf("F3");
+}
+else{
+printf("Nenhuma condição foi atendida");
+}}
