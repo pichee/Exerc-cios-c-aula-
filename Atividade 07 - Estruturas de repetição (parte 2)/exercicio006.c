47ccdd566182135768eafbf64bbe4f559443c5ea
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-26 19:58:39 -0300

    Update exercicio006.c

--- Atividade 07 - Estruturas de repetição (parte 2)/exercicio006.c
+++ Atividade 07 - Estruturas de repetição (parte 2)/exercicio006.c
@@ -1,18 +1,30 @@
-//Elaborar um programa que leia 15 valores numéricos inteiros positivos e apresente a soma dos fatoriais destes números
-//fatoriais destes números.
 #include <stdio.h>
-    int main(){
-        int c=0,soma=1,valor,fatorial,i=1;
-      while(c<=15)
-      {
-        printf("Digite um valor:");
-        scanf("%i",&valor);
-        fatorial=0;
-        while (i<=valor){
-            fatorial=fatorial*i;
+
+int main() {
+    int c = 0;
+    int soma = 0;
+    int valor, fatorial, i;
+
+    while (c < 15) {
+        printf("Digite um valor: ");
+        scanf("%d", &valor);
+
+
+        fatorial = 1;
+        i = 1;
+
+        while (i <= valor) {
+            fatorial *= i;
             i++;
         }
-        soma=soma+fatorial;
+
+
+        soma += fatorial;
+
         c++;
-        }
-        }
+    }
+
+    printf("A soma dos fatoriais é: %d\n", soma);
+
+    return 0;
+}
