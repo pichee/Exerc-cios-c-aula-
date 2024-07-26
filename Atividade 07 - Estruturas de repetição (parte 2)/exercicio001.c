a170a4f1c24a2944b53652eed4710a0a648fdade
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-26 19:20:07 -0300

    Create exercicio001.c

---
+++ Atividade 07 - Estruturas de repetição (parte 2)/exercicio001.c
@@ -0,0 +1,13 @@
+//Escreva um programa que calcule a soma dos números pares entre 1 e 100
+#include <stdio.h>
+int main(){
+    int i=0,soma;
+    printf("Numeros pares até 100:");
+    for(i=0;i<100;i++){
+        if (i%2==0){
+            printf("%i ",i);
+            soma+=i;
+        }
+    }
+    printf("A ssoma desses numeros é igual a %i",soma);
+}
