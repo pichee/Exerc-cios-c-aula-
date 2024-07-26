69a4d1b282f683312c614db12b4280ff80a4b3fb
Gustavo <76635390+pichee@users.noreply.github.com>
2024-07-26 20:06:12 -0300

    Create exercicio008.c

---
+++ Atividade 07 - Estruturas de repetição (parte 2)/exercicio008.c
@@ -0,0 +1,20 @@
+//Elaborar um programa que efetue a leitura de valores positivos inteiros até que um valor negativo 
+//seja informado. No final da execução do programa, devem ser apresentados o maior e o menor 
+//valor lido.
+#include <stdio.h>
+    int main(){
+        int maior=0,menor=99999999,i=0;
+        while(i>0){
+            printf("Digte um numero");
+            scanf("%i",&i);
+            if (i>maior){
+                maior=i;
+            }
+            if (i<menor){
+                maenor=i;
+            }
+            
+            
+        }
+        printf("%i maior numero,menor numero %i",maior,menor);
+    }
