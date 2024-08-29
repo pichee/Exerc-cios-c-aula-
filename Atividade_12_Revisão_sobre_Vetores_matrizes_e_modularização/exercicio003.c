3b702f16a83cd69bb8d4e9e1f553f0302a397f69
Gustavo <76635390+pichee@users.noreply.github.com>
2024-08-29 09:58:42 -0300

    Atualizar o exercicio003.c

--- Atividade_12_Revisão_sobre_Vetores_matrizes_e_modularização/exercicio003.c
+++ Atividade_12_Revisão_sobre_Vetores_matrizes_e_modularização/exercicio003.c
@@ -8,26 +8,54 @@ O programa deve conter uma estrutura de caso para cada opção e uma sub-rotina 
 para executar cada uma das tarefas*/
 #include <stdio.h>
 #include <stdlib.h>
-int prenchimento(){
+int prenchimento(int *vetor){
     int i=0;
     for (i = 0; i < 10; i++)
     {
         vetor[i]=rand()%100+1;
+    }
+}
+int mostrar(int *vetor){
+    int i=0;
+    for (i = 0; i < 10; i++)
+    {
         printf("%i ",vetor[i]);
     }
-    
+    printf("\n");
 }
+int inverter(int *vetor){
+    int i=0;
+    int aux;
+    
+    for (i =0; i<9; i++)
+    {
+    aux=vetor[i];
+        vetor[i]=vetor[i+1];
+        vetor[i+1]=aux;
+    }
+    printf("\n");
+}
+
+
     int main(){
-        int escolha=0,vetor[10];
-        printf("===============PROGRAMA===============\n1. Preencher o vetor com valores aleatórios\n2. Apresentar o vetor\n3. Inverter valores\n0. Encerrar programa\n");
+    int vetor[10]={0};
+    while(1){
+        int escolha=0;
+        printf("===============PROGRAMA===============\n1. Preencher o vetor com valores aleatórios\n2. Apresentar o vetor\n3. Inverter valores\n4. Encerrar programa\n");
         scanf("%i",&escolha);
         switch (escolha)
         {
         case 1:
-            prenchimento(vetor[10]);
+            prenchimento(vetor);
             break;
-        
-        default:
-            break;
+        case 2:
+         mostrar(vetor);
+         break;
+         case 3:
+         inverter(vetor);
+         break;
+        case 4:
+        return 0;
+         break;
         }
-    }
+    }}
