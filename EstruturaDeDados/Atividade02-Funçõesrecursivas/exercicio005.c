cff4ab7992b379c3c0f83ce162f8d4de7456aa5a
Gustavo <76635390+pichee@users.noreply.github.com>
2024-10-11 12:38:08 -0300

    Atualizar o Atividade02-Funçõesrecursivas

---
+++ EstruturaDeDados/Atividade02-Funçõesrecursivas/exercicio005.c
@@ -0,0 +1,20 @@
+/*
+Escreva uma função recursiva que receba um número inteiro positivo N e imprima todos os números 
+naturais de 0 (zero) até N em ordem crescente.
+*/
+#include <stdio.h>
+int i=0,k=0;
+int numero=0,penultimo=1,ultimo=1,aux;
+int main(){
+while(i==0){
+      printf("Digite um numero");
+      scanf("%i",&numero);
+      break;
+}
+printf("%i ",i);
+i++;
+if (i>numero){
+return 0;
+}
+main();
+}
