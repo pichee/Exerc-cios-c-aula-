0c42541c8306aa9926386c23716cc4783a065f6b
Gustavo <76635390+pichee@users.noreply.github.com>
2024-10-11 11:12:32 -0300

    Atualizar o Atividade02-Funçõesrecursivas

---
+++ EstruturaDeDados/Atividade02-Funçõesrecursivas/exercicio002.c
@@ -0,0 +1,24 @@
+/*
+Escreva um programa que contenha uma função que calcule a soma dos primeiros N cubos: ?(?) = 1! +
+2! + ⋯ + ?!
+*/
+#include <stdio.h>
+//i vai ser utilizado também para a soma
+int numeronaterceira=0;
+int i=0;
+int numero=0,soma=0;
+int main(){
+while(i==0){
+      printf("Digite um numero:");
+      scanf("%i",&numero);
+      break;
+}
+numeronaterceira=i*i*i;
+if(i>numero){
+ printf("A soma dos números é %i",soma);
+return 0;
+}
+i++;
+soma=soma+numeronaterceira;
+main();
+}
