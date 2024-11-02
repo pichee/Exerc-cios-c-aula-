5b812e97e4bb05602a8a0906321ee93eee2e331f
Gustavo <76635390+pichee@users.noreply.github.com>
2024-11-02 11:24:57 -0300

    Create exercicio007.c

---
+++ EstruturaDeDados/Atividade_05-Revisão/exercicio007.c
@@ -0,0 +1,8 @@
+/*
+ Assumindo que o endereço de uma variável num foi atribuído a um ponteiro pnum, quais das 
+seguintes expressões são verdadeiras? 
+a) num == &pnum 
+b) num == *pnum 
+c) pnum == *num 
+d) pnum == &num
+*/
